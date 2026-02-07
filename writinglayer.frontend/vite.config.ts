f82cce8a0e6063cc462784d338222e69d958dc59
Evaped <eva.pedersen90@gmail.com>
2026-02-07 09:38:21 +0100

    installed vite

--- writinglayer.frontend/vite.config.ts
+++ writinglayer.frontend/vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 60275,
-    }
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/test/setup.ts',
+    },
 })
