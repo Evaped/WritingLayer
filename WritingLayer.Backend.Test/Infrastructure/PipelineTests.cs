a2f429a19de8447b7ca6d80d2b50f9f8f6dfba61
Evaped <eva.pedersen90@gmail.com>
2026-02-07 19:53:54 +0100

    added pipeline for c# and python

---
+++ WritingLayer.Backend.Test/Infrastructure/PipelineTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WritingLayer.Backend.Test.Infrastructure;
+
+public class PipelineTests
+{
+    [Fact]
+    public void Pipeline_Should_Successfully_Run_Tests()
+    {
+        // This test ensures that the test runner is working 
+        // and the CI pipeline can execute C# code.
+        bool isSystemRunning = true;
+
+        Assert.True(isSystemRunning, "The basic test environment should be functional.");
+    }
+}
---
+++ WritingLayer.Backend.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace WritingLayer.Backend.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
